566244f548788502623a599b787911d8f8b3a1c2
ARPIT <ar123.jh@gmail.com>
2021-10-12 20:11:10 +0530

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,39 @@
+import React, {useState, useEffect } from 'react';
+import { Container , AppBar , Typography , Grow, Grid } from '@material-ui/core';
+import { useDispatch } from 'react-redux';
+import { getPosts } from './actions/posts';
+import ARSW from './images/ARSW.jpg'; 
+import  Form from './components/Form/Form';
+import Posts from './components/Posts/Posts';
+import useStyles from './styles';
+
+const App = () => {
+  const [currentId, setCurrentId] = useState(null);
+  const classes= useStyles();
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getPosts());
+  },[currentId,dispatch]);
+  return (
+    <Container maxidth="lg">
+      <AppBar className={classes.appBar} position="static" color="inherit">
+        <Typography className={classes.heading} variant="h2" align="center">A . R . Social World</Typography>
+        <img className={classes.image} src={ARSW} alt="A.R.S.W" height="60"/>
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid className={classes.mainContainer} container  justify="space-between" alignItems="stretch" spacing={3}>
+            <Grid item xs={12} sm={7}>
+              <Posts setCurrentId={setCurrentId} />
+            </Grid>
+            <Grid item xs={12} sm={4}>
+                <Form currentId={currentId} setCurrentId={setCurrentId}/>
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+    </Container>
+  );
+}
+
+export default App;
